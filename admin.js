b297a67eea65419c18c7418bb3079c15a57fb914
shashikantsharma80515-gif <shashikantsharma80515@gmail.com>
2026-07-06 12:03:46 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,11 @@
 function login() {
-    alert("JavaScript is working!");
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if (username === "admin" && password === "123456") {
+        alert("Login Successful ✅");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Wrong Username or Password ❌");
+    }
 }
