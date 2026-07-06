dec4b16ec5bfdeef8137f64230ff7e99a4de285a
shashikantsharma80515-gif <shashikantsharma80515@gmail.com>
2026-07-06 08:27:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Searching: " + searchBox.value);
     });
 });
+const searchInput = document.querySelector("input");
+const businesses = document.querySelectorAll(".business");
+
+searchInput.addEventListener("keyup", function () {
+  const value = searchInput.value.toLowerCase();
+
+  businesses.forEach(function (business) {
+    if (business.innerText.toLowerCase().includes(value)) {
+      business.style.display = "block";
+    } else {
+      business.style.display = "none";
+    }
+  });
+});
