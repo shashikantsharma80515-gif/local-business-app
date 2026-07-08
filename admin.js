4364426b2dbe7e65af9852266a25e794c2c18c6e
shashikantsharma80515-gif <shashikantsharma80515@gmail.com>
2026-07-08 05:37:01 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -3,8 +3,13 @@ function login() {
     let password = document.getElementById("password").value.trim();
 
     if (username === "admin" && password === "123456") {
+
+        localStorage.setItem("loggedIn", "true");
+
         alert("Login Successful ✅");
+
         window.location.href = "dashboard.html";
+
     } else {
         alert("Wrong Username or Password ❌");
     }
