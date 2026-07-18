1519bb50ae39df7bf337d7144ad177f426cbc59e
shashikantsharma80515-gif <shashikantsharma80515@gmail.com>
2026-07-18 09:56:16 +0530

    Update add-business.js

--- add-business.js
+++ add-business.js
@@ -1,33 +1,3 @@
-let businesses = JSON.parse(localStorage.getItem("businesses")) || [];
-
-let box = document.getElementById("businessList");
-
-if (businesses.length === 0) {
-    box.innerHTML = "<h3>No Business Found 😔</h3>";
-} else {
-
-    let html = "";
-
-    businesses.forEach(function(business) {
-
-        html += `
-        <div style="background:white;padding:15px;margin:15px;border-radius:10px;box-shadow:0 2px 8px rgba(0,0,0,.1);">
-
-        <h2>${business.businessName}</h2>
-
-        <p><b>Owner:</b> ${business.ownerName}</p>
-
-        <p><b>Phone:</b> ${business.phone}</p>
-
-        <p><b>Address:</b> ${business.address}</p>
-
-        <p><b>Category:</b> ${business.category}</p>
-
-        <hr>
-
-        </div>
-        `;
-    });
-
-    box.innerHTML = html;
+function saveBusiness() {
+    alert("Button Clicked ✅");
 }
