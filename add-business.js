312cf45ed04a0fdad840e395e7a1b4cbf2928df6
shashikantsharma80515-gif <shashikantsharma80515@gmail.com>
2026-07-23 10:49:35 +0530

    Update add-business.js

--- add-business.js
+++ add-business.js
@@ -1,8 +1,9 @@
 import { db } from "./firebase.js";
+
 import {
-  collection,
-  addDoc,
-  serverTimestamp
+    collection,
+    addDoc,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 async function saveBusiness() {
@@ -10,8 +11,12 @@ async function saveBusiness() {
     let businessName = document.getElementById("businessName").value.trim();
     let ownerName = document.getElementById("ownerName").value.trim();
     let phone = document.getElementById("phone").value.trim();
+    let email = document.getElementById("email").value.trim();
     let address = document.getElementById("address").value.trim();
+    let mapLink = document.getElementById("mapLink").value.trim();
+    let website = document.getElementById("website").value.trim();
     let category = document.getElementById("category").value;
+    let description = document.getElementById("description").value.trim();
 
     if (
         businessName === "" ||
@@ -19,7 +24,7 @@ async function saveBusiness() {
         phone === "" ||
         address === ""
     ) {
-        alert("Please fill all fields!");
+        alert("Please fill all required fields!");
         return;
     }
 
@@ -30,13 +35,21 @@ async function saveBusiness() {
     try {
 
         await addDoc(collection(db, "businesses"), {
-            businessName: businessName,
-            ownerName: ownerName,
-            phone: phone,
-            address: address,
-            category: category,
+
+            businessName,
+            ownerName,
+            phone,
+            email,
+            address,
+            mapLink,
+            website,
+            category,
+            description,
+
             status: "Pending",
+
             createdAt: serverTimestamp()
+
         });
 
         alert("Business Saved Successfully ✅");
